2acf0e11e7df1c2ac37e093f9a9d0f4307877d48
Julia Crosara <47763769+julia-crosara@users.noreply.github.com>
2020-09-25 18:57:56 -0700

    Add files via upload

--- colorGame.js
+++ colorGame.js
@@ -68,7 +68,7 @@ function reset(){
 			squares[i].style.display = "none";
 		}
 	}
-	h1.style.background = "steelblue";
+    h1.style.background = "slategray";
 }
 
 resetButton.addEventListener("click", function(){
